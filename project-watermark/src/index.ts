3e1b60f735e14e1c36e38ddfa57559c562a8b913
PiachkoAliaksey <alek17y@mail.ru>
2023-09-19 23:13:59 +0300

    add: index.ts

---
+++ project-watermark/src/index.ts
@@ -0,0 +1,7 @@
+import './style.scss'
+import { renderHTMLContent } from './RenderHTMLContent/renderHTMLContent';
+import { mainComponent } from './MainComponent/mainComponent';
+
+renderHTMLContent();
+
+mainComponent();
